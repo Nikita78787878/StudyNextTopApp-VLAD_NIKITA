3ec9b3bf84906efa2f530202c61319ffa2d8a3ad
Epov Nikita <Nikita.Epov@develonica.ru>
2025-08-12 12:53:08 +0800

    16.5 Проброс ref

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,10 +1,10 @@
 import {InputProps} from "./Input.props";
-import {JSX} from "react";
+import {ForwardedRef, forwardRef, JSX} from "react";
 import styles from './Input.module.css';
 import cn from "classnames";
 
-export const Input = ({className, ...props}: InputProps): JSX.Element => {
+export const Input = forwardRef(({className, ...props}: InputProps, ref: ForwardedRef<HTMLInputElement>):  JSX.Element => {
     return (
-        <input className={cn(className, styles.input)} {...props}/>
+        <input className={cn(className, styles.input)} ref={ref} {...props}/>
     )
-};
+});
--- components/Textarea/Textarea.tsx
+++ components/Textarea/Textarea.tsx
@@ -1,10 +1,10 @@
 import {TextareaProps} from "./Textarea.props";
-import {JSX} from "react";
+import {ForwardedRef, forwardRef, JSX} from "react";
 import styles from './Textarea.module.css';
 import cn from "classnames";
 
-export const Textarea = ({className, ...props}: TextareaProps): JSX.Element => {
+export const Textarea = forwardRef(({className, ...props}: TextareaProps, ref: ForwardedRef<HTMLInputElement>): JSX.Element => {
     return (
-        <textarea className={cn(className, styles.input)} {...props}/>
+        <textarea className={cn(className, styles.input)} ref={ref} {...props}/>
     )
-};
+});
