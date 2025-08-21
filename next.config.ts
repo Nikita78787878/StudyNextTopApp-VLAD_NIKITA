a4769a58f6e5a8312e965a3c9838a72d51d8516c
Epov Nikita <Nikita.Epov@develonica.ru>
2025-08-21 11:28:09 +0800

    19.6 Next export, вернул конфиг, т.к. сайт отображает не все папки при SSG

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,52 @@ import type { Configuration, RuleSetRule } from "webpack";
 
 const nextConfig: NextConfig = {
     output: 'export',
+};
+module.exports = nextConfig;
+
+const customWebpack = (config: Configuration): Configuration => {
+    // Убедимся, что module и rules существуют
+    if (!config.module || !config.module.rules) {
+        return config;
+    }
+
+    const rules = config.module.rules as RuleSetRule[];
+
+    // Найдем правило для SVG
+    const fileLoaderRule = rules.find((rule) => {
+        return (
+            rule &&
+            typeof rule !== "string" &&
+            rule.test instanceof RegExp &&
+            rule.test.test(".svg")
+        );
+    }) as RuleSetRule | undefined;
+
+    if (!fileLoaderRule) {
+        return config;
+    }
+
+    // Удалим старое правило (если нужно) или модифицируем напрямую
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    rules.push(
+        {
+            ...fileLoaderRule,
+            test: /\.svg$/i,
+            resourceQuery: /url/, // svg?url
+        },
+        {
+            test: /\.svg$/i,
+            issuer: fileLoaderRule.issuer ?? undefined,
+            resourceQuery: { not: [/url/] },
+            use: ['@svgr/webpack'],
+        }
+    );
+
+    return config;
+};
+
+module.exports = {
     images: {
         remotePatterns: [
             {
@@ -12,47 +58,8 @@ const nextConfig: NextConfig = {
             },
         ],
     },
-    webpack: (config: Configuration): Configuration => {
-        // Убедимся, что module и rules существуют
-        if (!config.module || !config.module.rules) {
-            return config;
-        }
-
-        const rules = config.module.rules as RuleSetRule[];
-
-        // Найдем правило для SVG
-        const fileLoaderRule = rules.find((rule) => {
-            return (
-                rule &&
-                typeof rule !== "string" &&
-                rule.test instanceof RegExp &&
-                rule.test.test(".svg")
-            );
-        }) as RuleSetRule | undefined;
-
-        if (!fileLoaderRule) {
-            return config;
-        }
-
-        // Удалим старое правило (если нужно) или модифицируем напрямую
-        fileLoaderRule.exclude = /\.svg$/i;
-
-        rules.push(
-            {
-                ...fileLoaderRule,
-                test: /\.svg$/i,
-                resourceQuery: /url/, // svg?url
-            },
-            {
-                test: /\.svg$/i,
-                issuer: fileLoaderRule.issuer ?? undefined,
-                resourceQuery: { not: [/url/] },
-                use: ['@svgr/webpack'],
-            }
-        );
-
-        return config;
-    },
+    webpack: customWebpack,
 };
+
 
 export default nextConfig;
